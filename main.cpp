1fc165b48a700fb74c7a7b2b966bd67bdf38cca4
Annnnya <anna.polova@ucu.edu.ua>
2023-10-16 15:43:54 +0300

    command history

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@
 #include <sstream>
 #include <cstring>
 #include <limits.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-// Function to tokenize a string into a vector of arguments
 std::vector<std::string> tokenize(const std::string& input) {
     std::vector<std::string> tokens;
     std::string token;
@@ -20,27 +21,23 @@ std::vector<std::string> tokenize(const std::string& input) {
     return tokens;
 }
 
-// Execute external commands using fork and exec
 int executeCommand(const std::vector<std::string>& args) {
     pid_t pid = fork();
     if (pid == -1) {
         std::cerr << "Fork failed." << std::endl;
         return -1;
-    } else if (pid == 0) { // Child process
-        // Convert vector of strings to array of C-style strings
+    } else if (pid == 0) {
         std::vector<char*> c_args;
         for (const std::string& arg : args) {
             c_args.push_back(const_cast<char*>(arg.c_str()));
         }
         c_args.push_back(nullptr);
 
-        // Execute the command
         execvp(c_args[0], c_args.data());
 
-        // If execvp fails, print an error message
         std::cerr << "Command not found: " << args[0] << std::endl;
         exit(EXIT_FAILURE);
-    } else { // Parent process
+    } else {
         int status;
         waitpid(pid, &status, 0);
         if (WIFEXITED(status)) {
@@ -52,43 +49,40 @@ int executeCommand(const std::vector<std::string>& args) {
 
 int main() {
     std::string input;
-    std::string prompt = " $ "; // Set a default prompt without the path
 
     while (true) {
-        // Get the current working directory
-        char cwd[PATH_MAX]; // PATH_MAX is a system-defined constant
+        char cwd[PATH_MAX];
         if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            // Display the prompt with the current working directory
-            std::cout << cwd << prompt;
+            std::string prompt = " " + std::string(cwd) + " $ ";
+            char* userInput = readline(prompt.c_str());
+
+            if (userInput == nullptr) {
+                break; // Exit on Ctrl+D
+            }
+
+            input = userInput;
+            free(userInput);
+
+            if (!input.empty()) {
+                add_history(input.c_str());
+                std::vector<std::string> args = tokenize(input);
+
+                if (args[0] == "exit") {
+                    break;
+                } else if (args[0] == "cd" && args.size() > 1) {
+                    if (chdir(args[1].c_str()) != 0) {
+                        std::cerr << "Error changing directory" << std::endl;
+                    }
+                } else {
+                    int status = executeCommand(args);
+                    if (status == -1) {
+                        std::cerr << "Command failed." << std::endl;
+                    }
+                }
+            }
         } else {
-            // Error handling if getcwd fails
             std::cerr << "Error getting current working directory" << std::endl;
             break;
-        }
-
-        // Read a line of input from the user
-        std::getline(std::cin, input);
-
-        // Tokenize the input into arguments
-        std::vector<std::string> args = tokenize(input);
-
-        if (args.empty()) {
-            continue; // Empty input, prompt again
-        }
-
-        // Check for built-in commands
-        if (args[0] == "exit") {
-            break; // Exit the shell
-        } else if (args[0] == "cd" && args.size() > 1) {
-            if (chdir(args[1].c_str()) != 0) {
-                std::cerr << "Error changing directory" << std::endl;
-            }
-        } else {
-            // Execute external commands
-            int status = executeCommand(args);
-            if (status == -1) {
-                std::cerr << "Command failed." << std::endl;
-            }
         }
     }
 
